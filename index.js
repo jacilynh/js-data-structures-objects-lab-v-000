be809fa1cfd7db03accec8d3e8061c7d2eaad9cb
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-18 18:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
+// defines a `driver` driver 
 // drivers
 
-// defines a `driver` driver ‣
-// AssertionError: expected 'undefined' to equal 'object'
-//     at Context.<anonymous> (test/indexTest.js:5:30)
 
+// "before each" hook for "returns an driver with the original key value pairs and the new key value pair" :
 // updateDriverWithKeyAndValue(driver, key, value)
 
-// "before each" hook for "returns an driver with the original key value pairs and the new key value pair" ‣
-// ReferenceError: driver is not defined
-//     at Context.<anonymous> (test/indexTest.js:10:25)
 
-
-
-// updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver ‣
+// updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver:
 // destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
 
-// deleteFromDriverByKey(driver, key)
-// deletes `key` from a clone of driver and returns the new driver (it is non-destructive) ‣
-// ReferenceError: deleteFromDriverByKey is not defined
-//     at Context.<anonymous> (test/indexTest.js:47:23)
-// does not modify the original driver (it is non-destructive) ‣
-// ReferenceError: deleteFromDriverByKey is not defined
-//     at Context.<anonymous> (test/indexTest.js:53:7)
 
+// deletes `key` from a clone of driver and returns the new driver (it is non-destructive), does not modify the original driver (it is non-destructive):
+// deleteFromDriverByKey(driver, key)
+
+
+// returns driver without the delete key/value pair, modifies the original driver:
 // destructivelyDeleteFromDriverByKey(driver, key)
 
-// returns driver without the delete key/value pair ‣
-// ReferenceError: destructivelyDeleteFromDriverByKey is not defined
-//     at Context.<anonymous> (test/indexTest.js:61:23)
-// modifies the original driver ‣
-// ReferenceError: destructivelyDeleteFromDriverByKey is not defined
-//     at Context.<anonymous> (test/indexTest.js:67:23)
