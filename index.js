7b173301717d521d3cb37bdb018dfd6276305c89
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-18 18:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
 // defines a `driver` driver ‣
 // AssertionError: expected 'undefined' to equal 'object'
 //     at Context.<anonymous> (test/indexTest.js:5:30)
+
 // updateDriverWithKeyAndValue(driver, key, value)
+
 // "before each" hook for "returns an driver with the original key value pairs and the new key value pair" ‣
 // ReferenceError: driver is not defined
 //     at Context.<anonymous> (test/indexTest.js:10:25)
+
 // destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+
 // updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver ‣
-// ReferenceError: destructivelyUpdateDriverWithKeyAndValue is not defined
-//     at Context.<anonymous> (test/indexTest.js:33:7)
+
+
 // deleteFromDriverByKey(driver, key)
 // deletes `key` from a clone of driver and returns the new driver (it is non-destructive) ‣
 // ReferenceError: deleteFromDriverByKey is not defined
@@ -18,7 +22,9 @@
 // does not modify the original driver (it is non-destructive) ‣
 // ReferenceError: deleteFromDriverByKey is not defined
 //     at Context.<anonymous> (test/indexTest.js:53:7)
+
 // destructivelyDeleteFromDriverByKey(driver, key)
+
 // returns driver without the delete key/value pair ‣
 // ReferenceError: destructivelyDeleteFromDriverByKey is not defined
 //     at Context.<anonymous> (test/indexTest.js:61:23)
